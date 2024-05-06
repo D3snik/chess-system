ad23e14ad27509601b89fcffcdf8bd4058606b6d
Ryan Linhares da Silva <ryandesnik45@gmail.com>
2024-05-06 13:08:01 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,8 +2,12 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+
+
 public abstract class ChessPiece extends Piece {
 	private Color color;
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -12,6 +16,17 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
